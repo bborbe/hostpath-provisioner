db6584e554729b424efde9581319365c10b6f4fc
Benjamin Borbe <bborbe@rocketnews.de>
2018-10-14 22:31:55 +0200

    Add Namespace and Pvc name to folder

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to this project will be documented in this file.
 
+## 1.1.0
+
+- Add Namespace and Pvc name to folder
+
 ## 1.0.0
 
 - Initial version
--- hostpath-provisioner.go
+++ hostpath-provisioner.go
@@ -73,7 +73,7 @@ var _ controller.Provisioner = &hostPathProvisioner{}
 
 // Provision creates a storage asset and returns a PV object representing it.
 func (p *hostPathProvisioner) Provision(options controller.VolumeOptions) (*v1.PersistentVolume, error) {
-	path := path.Join(p.pvDir, options.PVName)
+	path := path.Join(p.pvDir, options.PVC.Namespace+"-"+options.PVC.Name+"-"+options.PVName)
 
 	if err := os.MkdirAll(path, 0777); err != nil {
 		return nil, err
